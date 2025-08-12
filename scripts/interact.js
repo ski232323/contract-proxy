1fdf0b4dd45131d2d11fc508c955cd755b53cb14
Ulysse Renzetti <ulysse.renzetti@outlook.fr>
2025-08-12 20:01:19 +0200

    testing git stash

--- scripts/interact.js
+++ scripts/interact.js
@@ -1,6 +1,6 @@
 const {ethers} =require("hardhat")
 async function main() {
-    const contractAddress="0x0AFdd448e8E7f88b6e5f9a8d891e16c842743D28"
-    const Contract=await ethers.getContractFactory(contractAddress)
+    const contractAddress="0x0AFdd448e8E7f88b6e5f9a8d891e16c842743D28";
+    const Contract=await ethers.getContractFactory(contractAddress);
     
 }
